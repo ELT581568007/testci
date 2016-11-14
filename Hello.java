89dddaf2e2e9c234db91ad1b884e458705c4e269
ELT581568007 <wongsathorn.yin@crru.ac.th>
2016-11-14 15:08:54 +0700

    init file(dfdf)

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
-	public static void main(String args[]) s
+	public static void main(String args[]) {
 		System.out.println("Hello world");
+		System.out.println("Hello travis");
 	}
 }
